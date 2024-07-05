ffd614be7d8c9c8ffd670516aa4acc751b8dcc4f
leftrightmidnight <matinoghabi@outlook.com>
2024-07-05 17:29:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function loadGame(gameName) {
             gameEmbedCode = '<iframe src="https://www.gameflare.com/embed/bob-the-robber-4-japan/" frameborder="0" scrolling="no" width="800" height="635" allowfullscreen></iframe>';
             break;
         case 'bob-the-robber-5':
-            gameEmbedCode = '<iframe src="https://www.gameflare.com/embed/bob-the-robber-5-temple-adventure/" frameborder="0" scrolling="no" width="765" height="545" allowfullscreen></iframe>';
+            gameEmbedCode = '<iframe src="https://html5.gamedistribution.com/3c48eb1a56064a83b3c2ca0acbe01bb2/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}" width="800" height="600" scrolling="none" frameborder="0"></iframe>';
             break;
         case 'papas-hot-doggeria':
             gameEmbedCode = '<iframe src="https://www.crazygames.com/embed/papas-hotdoggeria" style="width: 100%; height: 100%;" frameborder="0" allow="gamepad *;"></iframe>';
